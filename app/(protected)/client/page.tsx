9fc38ee35ed7c90332cea231767d808d8a2686a3
Wood1208 <155505564+Wood1208@users.noreply.github.com>
2024-12-15 10:16:38 +0800

    First commit

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const ClientPage = () => {
+  const user = useCurrentUser();
+
+  return (
+    <UserInfo
+      label="Client components"
+      user={user}
+    />
+  );
+}
+
+export default ClientPage;
